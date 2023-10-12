96c14f740a7dc28013bf9b15a752b4004f9cc200
abayalii <abayalisan@gmail.com>
2023-10-12 17:03:25 +0300

    Update project3.sql

--- project3.sql
+++ project3.sql
@@ -1,13 +1,13 @@
-/*SELECT country FROM country
+SELECT country FROM country
 WHERE country LIKE 'A%a';*/
 
-/*SELECT country FROM country
-WHERE country LIKE '_____n';*/
+SELECT country FROM country
+WHERE country LIKE '_____n';
 
-/*SELECT title FROM film
-WHERE title ILIKE '%T%T%T%T%';*/
+SELECT title FROM film
+WHERE title ILIKE '%T%T%T%T%';
 
-/*SELECT * FROM film
-WHERE title LIKE 'C%' AND length>90 AND rental_rate=2.99;*/
+SELECT * FROM film
+WHERE title LIKE 'C%' AND length>90 AND rental_rate=2.99;
 
 
