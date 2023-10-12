af354cec07d7f553f531330f9a0641be62aeee24
Alişan Abay <126713304+abayalii@users.noreply.github.com>
2023-10-12 17:35:40 +0300

    Update project4.sql

--- project4.sql
+++ project4.sql
@@ -1,11 +1,11 @@
---SELECT DISTINCT replacement_cost FROM film
---SELECT COUNT(DISTINCT replacement_cost) FROM film
+SELECT DISTINCT replacement_cost FROM film
+SELECT COUNT(DISTINCT replacement_cost) FROM film
 
-/*SELECT COUNT(title) FROM film
-WHERE title LIKE 'T%' AND rating='G';*/
+SELECT COUNT(title) FROM film
+WHERE title LIKE 'T%' AND rating='G';
 
-/*SELECT COUNT(country) FROM country
-WHERE country LIKE '_____';*/
+SELECT COUNT(country) FROM country
+WHERE country LIKE '_____';
 
-/*SELECT COUNT(city) FROM city
-WHERE city ILIKE '%r';*/
+SELECT COUNT(city) FROM city
+WHERE city ILIKE '%r';
