8fbc4fa825f811fac7d964a72172fbe909dee344
abayalii <abayalisan@gmail.com>
2023-10-12 20:23:57 +0300

    Create project7.sql

---
+++ project7.sql
@@ -0,0 +1,15 @@
+SELECT rating,COUNT(rating) FROM film
+GROUP BY rating
+ORDER BY COUNT(rating);
+
+SELECT replacement_cost, COUNT(replacement_cost) FROM film
+GROUP BY replacement_cost 
+HAVING COUNT(replacement_cost)>50;
+
+SELECT store_id,COUNT(store_id) FROM customer
+GROUP BY store_id;
+
+SELECT country_id,COUNT(*) FROM city
+GROUP BY country_id 
+ORDER BY COUNT(*) DESC
+LIMIT 1;
