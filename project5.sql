75318304a9aa46f18369fd2b62ef8f2293cd47cd
abayalii <abayalisan@gmail.com>
2023-10-12 19:18:17 +0300

    Create project5.sql

---
+++ project5.sql
@@ -0,0 +1,15 @@
+/*SELECT title,length FROM film
+WHERE title LIKE '%n'
+ORDER BY length DESC
+limit 5;*/
+
+/*SELECT title,length FROM film
+WHERE title LIKE '%n'
+ORDER BY length ASC
+OFFSET 5
+LIMIT 5;*/
+
+/*SELECT * FROM customer
+where store_id=1
+ORDER BY last_name DESC
+LIMIT 4;*/
